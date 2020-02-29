a7ce76399c300be325457aa8e9f734351c06fcb6
safari <safari@mail>
2020-02-29 15:35:35 +0330

    add ngbootstrap modal

--- AngularCore/ClientApp/src/app/navbar/navbar.component.ts
+++ AngularCore/ClientApp/src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, TemplateRef } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { APP_CONFIG, AppConfig } from 'app/app.config';
 
@@ -17,7 +18,7 @@ export class NavbarComponent implements OnInit {
   title: string;
   version: string;
 
-  constructor(private http: HttpClient, @Inject(APP_CONFIG) private config: AppConfig) { }
+  constructor(private http: HttpClient, private modalService: NgbModal, @Inject(APP_CONFIG) private config: AppConfig) { }
 
   ngOnInit() {
     this.title = this.config.title;
@@ -29,6 +30,15 @@ export class NavbarComponent implements OnInit {
       this.navbarCollapsed = true;
       const url = `${this.config.apiEndpoint}Authenticate/LogOut`;
       const promise = await this.http.get<HttpResponse<{}>>(url).toPromise();
+    } catch (e) {
+      console.log('error', e);
+    }
+  }
+
+  async changePassword(content: TemplateRef<any>): Promise<void> {
+    try {
+      this.navbarCollapsed = true;
+      this.modalService.open(content, { centered: true });
     } catch (e) {
       console.log('error', e);
     }
