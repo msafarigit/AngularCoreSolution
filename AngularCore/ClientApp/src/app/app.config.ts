22eb3b1ce74b0c32b764836d54fec2ee15cae879
safari <safari@edsab.co>
2020-04-28 13:32:13 +0430

    update ngx-toastr, ngx-cookie-service

--- AngularCore/ClientApp/src/app/app.config.ts
+++ AngularCore/ClientApp/src/app/app.config.ts
@@ -15,7 +15,7 @@ export const APP_DI_CONFIG: AppConfig = {
   title: 'سامانه نیما',
   version: '3.0.1.0',
   arcRestBaseUrl: 'http://192.168.0.129:6080/arcgis/rest/services',
-  arcBaseUrl: 'http://192.168.0.129/arcgis_js_v414_api/arcgis_js_api/library/4.14',
+  arcBaseUrl: 'http://192.168.0.129:8000/arcgis_js_v414_api/arcgis_js_api/library/4.14',
   arcApiEndpoint: 'api.arc.com',
   apiEndpoint: '/api/'
 };
