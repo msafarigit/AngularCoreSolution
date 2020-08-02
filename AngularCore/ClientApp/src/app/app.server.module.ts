23b81a0a9f00bcaa29a843c2e266e0e4f24a2643
msafarigit <msafari.int@gmail.com>
2020-08-02 11:53:30 +0430

    remove @nguniversal/module-map-ngfactory-loader from packages to update angular 10

--- AngularCore/ClientApp/src/app/app.server.module.ts
+++ AngularCore/ClientApp/src/app/app.server.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+// import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'; update to angular 10
 import { AppComponent } from '@component/app.component';
 import { AppModule } from './app.module';
 
 @NgModule({
-    imports: [AppModule, ServerModule, ModuleMapLoaderModule],
+    // imports: [AppModule, ServerModule, ModuleMapLoaderModule], update to angular 10
+    imports: [AppModule, ServerModule],
     bootstrap: [AppComponent]
 })
 export class AppServerModule { }
